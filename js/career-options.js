9a5942142c6fb7bf1e5894516590bfe9e9c9b27b
Paul <paul.ziolkowski@student.cart.org>
2023-02-26 17:05:11 -0800

    Rework career options code

--- js/career-options.js
+++ js/career-options.js
@@ -1,29 +1,51 @@
 import { data } from './EECU-data.js';
 
-const currencyFormatter = Intl.NumberFormat(undefined, {
+const formatter = Intl.NumberFormat(undefined, {
     style: 'currency',
     currency: 'USD'
 });
 
-const element = document.getElementsByClassName('career-options-list')[0];
-const search = document.getElementById('search');
+function createListItem(name, salary) {
+    const encodedName = encodeURIComponent(name);
+    const formattedSalary = formatter.format(salary);
 
-search.addEventListener('input', () => {
-    const value = search.value;
-    for (const item of document.getElementsByClassName('career-options-list-item')) {
-        item.classList.remove('none');
-        if (item.getElementsByTagName('h3')[0].textContent.toLocaleLowerCase().trim().indexOf(value.toLocaleLowerCase().trim())) {
-            item.classList.add('none');
-        }
-    }
-});
+    const header = document.createElement('h3');
+    const headerText = document.createTextNode(name);
 
-for (const option of data) {
-    const [name, salary] = [...option];
-    const formattedSalary = currencyFormatter.format(salary);
-    element.innerHTML += `<a class="career-options-list-item flex column" href="/calculator.html?name=${encodeURIComponent(name)}&salary=${salary}">
-    <h3>${name}</h3>
-    <div class="bubble salary" title="Gross Annual Income">${formattedSalary}</div>
-    </a>
-    `;
+    header.appendChild(headerText);
+
+    const bubble = document.createElement('div');
+    const bubbleText = document.createTextNode(formattedSalary);
+
+    bubble.classList.add('bubble', 'salary');
+    bubble.title = 'Gross Annual Income';
+    bubble.appendChild(bubbleText);
+
+    const listItem = document.createElement('a');
+
+    listItem.classList.add('career-options-list-item', 'flex', 'column');
+    listItem.href = `/calculator.html?name=${encodedName}&salary=${salary}`;
+    listItem.append(header, bubble);
+
+    return listItem;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const careerOptionsList = document.getElementById('career-options-list');
+
+    const listItems = data.map(o => createListItem(...o));
+
+    careerOptionsList.append(...listItems);
+
+    const search = document.getElementById('search');
+
+    search.addEventListener('input', () => {
+        const value = search.value;
+        for (const item of document.getElementsByClassName('career-options-list-item')) {
+            item.classList.remove('none');
+            if (item.getElementsByTagName('h3')[0].textContent.toLocaleLowerCase().trim().indexOf(value.toLocaleLowerCase().trim())) {
+                item.classList.add('none');
+            }
+        }
+    });
+});
